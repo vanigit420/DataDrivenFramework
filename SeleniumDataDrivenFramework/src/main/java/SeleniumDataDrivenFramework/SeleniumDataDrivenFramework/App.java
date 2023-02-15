624eacf0f564a9e5cbbdebe052d6ffa5a0574454
vanigit420 <104908880+vanigit420@users.noreply.github.com>
2023-02-16 03:47:47 +0530

    Data driven framework

---
+++ SeleniumDataDrivenFramework/src/main/java/SeleniumDataDrivenFramework/SeleniumDataDrivenFramework/App.java
@@ -0,0 +1,13 @@
+package SeleniumDataDrivenFramework.SeleniumDataDrivenFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
